a95e69f24c995077b9fe9cd3745bdf000767d9ac
Handla <duofrancis11@gmail.com>
2026-04-15 05:37:07 +0000

    feat: Product views and templates created

--- makola/views.py
+++ makola/views.py
@@ -35,3 +35,33 @@ class ProfileDeleteView(ListView):
     context_object_name = 'profile'
     message = 'Are you sure you want to delete this profile?'
     success_url = '/profiles/'  # Redirect to profile list after successful deletion
+
+class ProductListView(ListView):
+    model = Profile
+    template_name = 'makola/product_list.html'
+    context_object_name = 'products'
+
+class ProductDetailView(ListView):
+    model = Profile
+    template_name = 'makola/product_detail.html'
+    context_object_name = 'product'
+
+class ProductCreateView(ListView):
+    model = Profile
+    template_name = 'makola/product_create.html'
+    context_object_name = 'product'
+    success_url = '/products/'  # Redirect to product list after successful creation
+
+class ProductUpdateView(ListView):
+    model = Profile
+    template_name = 'makola/product_update.html'
+    context_object_name = 'product'
+    success_url = '/products/'  # Redirect to product list after successful update
+
+class ProductDeleteView(ListView):
+    model = Profile
+    template_name = 'makola/product_delete.html'
+    context_object_name = 'product'
+    message = 'Are you sure you want to delete this product?'
+    success_url = '/products/'  # Redirect to product list after successful deletion
+    
