6c6b4d0e79e7c71d8276405d3388206821c080a1
Handla <duofrancis11@gmail.com>
2026-06-26 06:56:35 +0000

    New columns added to the Payment table

--- payments/models.py
+++ payments/models.py
@@ -23,10 +23,14 @@ class Payment(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    payer = models.CharField(max_length=50, default='Sender')
+    payee = models.CharField(max_length=100, default='Receiver')
+    description = models.TextField(blank=True, null=True, default='Payment for services rendered')
+    # reference = models.CharField(max_length=100, unique=True, default=uuid.uuid4, editable=False)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=PAYMENT_STATUS_CHOICES, default='pending')
     method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, default='cash')
 
     def __str__(self):
-        return f'Payment of ${self.amount} at {self.timestamp}'
+        return f'Payment of ${self.amount} from {self.payer} to {self.payee} at {self.timestamp}'
